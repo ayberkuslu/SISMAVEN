257fd1167e2f6a0028866d0f0d415d19a79daf4b
usluayberk <usluayberk1998@gmail.com>
2019-07-01 14:00:51 +0300

    getUser page for admin started

---
+++ src/main/java/Controllers/Admin/KullaniciGetirController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers.Admin;
+
+import Controllers.*;
+import Models.*;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author hp_user
+ */
+@Named(value = "kullaniciGetirController")
+@Dependent
+@ManagedBean
+@RequestScoped
+public class KullaniciGetirController extends Controller {
+
+    private List users;
+    private List userDetails;
+
+    /**
+     * Creates a new instance of KullaniciGetirController
+     */
+    public KullaniciGetirController() {
+    }
+
+    public void loadData() {
+        setSession(HibernateUtil.getSessionFactory().openSession());
+        getSession().beginTransaction();
+
+        userDetails = getSession().createCriteria(UserDetails.class).add(Restrictions.eq("userId.type", 2)).list();
+        users = getSession().createCriteria(Users.class).list();
+        System.out.println("User list size : " + users.size()+"\n detail size : " + userDetails.size());
+        getSession().close();
+
+    }
+
+}
